ef59ec06266a6b378c05af135860ef5b437c8744
YashadaKul <>
2024-04-07 16:39:39 +1000

    Finished assignment

--- WeatherPy/Starter_Code/.ipynb_checkpoints/api_keys-checkpoint.py
+++ WeatherPy/Starter_Code/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ec8d1256e1f9dadda39d4b65d57fae2d"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "21475421a8ab4660bcbfb493414dd270"
+geoapify_key = ""
